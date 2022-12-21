bb90174859c9a6f0b2c1a0f60297207daa116fb3
IMickisz <ivan.mickisz3@gmail.com>
2022-12-21 10:42:14 +0100

    corrected task 2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -6,7 +6,6 @@ def matrix_shape(matrix):
     """ Function that calculates the shape of a matrix """
     shape = []
     shape.append(len(matrix))
-    shape.append(len(matrix[0]))
-    if type(matrix[0][0]) == list:
-        shape.append(len(matrix[0][0]))
+    if type(matrix[0]) == list:
+        shape.append(len(matrix[0]))
     return shape
