b64406fa2a52ed106272389d601b5628f64f57fa
IMickisz <ivan.mickisz3@gmail.com>
2022-12-23 12:10:47 +0100

    added condition for derivative of 0 for task 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,6 +6,8 @@ def poly_derivative(poly):
     """ Function that calculates the derivative of a polynomial """
     if (type(poly) is not list) or poly == []:
         return None
+    elif len(poly) < 2:
+        return [0]
     else:
         deriv_poly = [poly[i] * i for i in range(1, len(poly))]
         return deriv_poly
