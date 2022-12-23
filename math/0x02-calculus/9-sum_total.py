7fa0a6e28463a385bad278a7ef8720d8e63dfdc0
IMickisz <ivan.mickisz3@gmail.com>
2022-12-23 11:55:07 +0100

    change None return conditions in task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """ Function that calculates the sum of i=1 until n of i² """
-    if (type(n) is not int) or (n is None) or (n < 1):
+    if (type(n) is not int) or (n < 1):
         return None
     else:
         result = map(lambda i: i ** 2, range(1, n + 1))
