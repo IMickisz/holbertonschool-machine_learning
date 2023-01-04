ddb75ab52d8e50da33ccaacd9e6c808dc69715cb
IMickisz <ivan.mickisz3@gmail.com>
2023-01-04 22:36:24 +0100

    added Tasks 1, 2 and 3

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Module that create a neuron """
+import numpy as np
+
+
+class Neuron:
+    """ Class that defines a single neuron performing binary classification """
+
+    def __init__(self, nx):
+        """
+        class constructor
+           - nx: is the number of input features to the neuron
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+        @property
+    def W(self):
+        """ Getter function for W that return the weight vector neuron """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter function for b that return the  bias for the neuron """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter function for W return the activated output of the neuron """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Method that calculates the forward propagation of the neuron
+            - X: a np array with shape (nx, m) that contains the input data
+        """
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
