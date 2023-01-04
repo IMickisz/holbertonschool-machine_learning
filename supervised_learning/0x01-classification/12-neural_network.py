41bf593dcfa7e00f680a63752e1db72e7e41d173
IMickisz <ivan.mickisz3@gmail.com>
2023-01-04 23:59:47 +0100

    added task 11

--- supervised_learning/0x01-classification/12-neural_network.py
+++ supervised_learning/0x01-classification/12-neural_network.py
@@ -8,27 +8,27 @@ class NeuralNetwork:
     binary classification """
 
     def __init__(self, nx, nodes):
-            """
-            class constructor
-            - nx : number of input features to the neuron
-            - nodes : number of nodes found in the hidden layer
-            """
-            if type(nx) is not int:
-                    raise TypeError("nx must be an integer")
-            if nx < 1:
-                    raise ValueError("nx must be a positive integer")
-            if type(nodes) is not int:
-                    raise TypeError("nodes must be an integer")
-            if nodes < 1:
-                    raise ValueError("nodes must be a positive integer")
-            # hidden layer
-            self.__W1 = np.random.randn(nodes, nx)
-            self.__b1 = np.zeros((nodes, 1))
-            self.__A1 = 0
-            # output neuron
-            self.__W2 = np.random.randn(1, nodes)
-            self.__b2 = 0
-            self.__A2 = 0
+        """
+        class constructor
+             - nx : number of input features to the neuron
+             - nodes : number of nodes found in the hidden layer
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        # hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        # output neuron
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
