d594cdf2ab972ee31fe2aeb03f520ade1a59b50c
IMickisz <ivan.mickisz3@gmail.com>
2023-01-06 23:58:18 +0100

    correction task 7

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -10,18 +10,13 @@ def evaluate(X, Y, save_path):
          - Y : numpy.ndarray containing the one-hot labels for X
          - save_path : location to load the model from
     """
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
-    sess.run(init)
-    saver = tf.train.import_meta_graph(save_path + '.meta')
-    saver.restore(sess, save_path)
-    x = tf.get_collection('x')[0]
-    y = tf.get_collection('y')[0]
-    y_pred = tf.get_collection('y_pred')[0]
-    loss = tf.get_collection('loss')[0]
-    accuracy = tf.get_collection('accuracy')[0]
-    y_predr = sess.run((y_pred), feed_dict={x: X, y: Y})
-    accuar = sess.run((accuracy), feed_dict={x: X, y: Y})
-    lossr = sess.run((loss), feed_dict={x: X, y: Y})
-    sess.close()
-    return y_predr, accuar, lossr
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        loss = tf.get_collection("loss")[0]
+        acc = tf.get_collection("accuracy")[0]
+        vars = sess.run([y_pred, acc, loss], feed_dict={x: X, y: Y})
+        return vars[0], vars[1], vars[2]
