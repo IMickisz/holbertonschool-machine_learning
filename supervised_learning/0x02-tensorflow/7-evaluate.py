e46b379e9b48958ff90116aa7a62ad51a935269e
IMickisz <ivan.mickisz3@gmail.com>
2023-01-06 23:48:28 +0100

    trying again for task 7

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -11,16 +11,14 @@ def evaluate(X, Y, save_path):
          - save_path : location to load the model from
     """
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
 
-        x = tf.get_collection("x")[0]
-        y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")[0]
-        loss = tf.get_collection("loss")[0]
-        accuracy = tf.get_collection("accuracy")[0]
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
 
-        eval_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
-        eval_loss = sess.run(loss, feed_dict={x: X, y: Y})
-        eval_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
-    return eval_pred, eval_loss, eval_accuracy
+        vars = sess.run([y_pred, acccuracy, loss], feed_dict={x: X, y: Y})
+        return vars[0], vars[1], vars[2]
