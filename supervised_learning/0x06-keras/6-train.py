6ba4d1c6837de74c665df9e33fc476ad683f6ac9
IMickisz <ivan.mickisz3@gmail.com>
2023-01-30 23:45:38 +0100

    removed additional parenthesis

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -37,5 +37,5 @@ def train_model(network, data, labels, batch_size, epochs,
     history = network.fit(
         data, labels, batch_size=batch_size, epochs=epochs,
         validation_data=validation_data, verbose=verbose,
-        callbacks=stopping, shuffle=shuffle))
+        callbacks=stopping, shuffle=shuffle)
     return history
