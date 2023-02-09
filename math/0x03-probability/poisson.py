5a3099867667aa556b4a5b5254ad0eb9735c28c9
IMickisz <ivan.mickisz3@gmail.com>
2023-02-09 15:40:16 +0100

    added tasks 0, 1 and 2

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" Module that contains the Poisson class """
+
+class Poisson:
+    """ Class  that represents a poisson distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Class constructor
+           - data is a list of the data to be used to estimate the distribution
+           - lambtha is the expected number of occurences in a given time frame
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Instance method that calculates the value of the PMF for a given number
+        of “successes”
+           - k: number of “successes”
+        Return: the PMF value for k
+        """
+        if type(k) != int:
+            k = int(k)
+        if k < 0:
+            return 0
+        e = 2.7182818285
+        k_factorial = 1
+        for i in range(2, k + 1):
+            k_factorial *= i
+        pmf = ((e ** (- self.lambtha)) * (self.lambtha ** k)) / k_factorial
+        return pmf
+
+    def cdf(self, k):
+        """
+        Instance method that calculates the value of the CDF for a given
+        number of “successes”
+           - k: number of “successes”
+        Return: the CDF value for k
+        """
+        if type(k) != int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
