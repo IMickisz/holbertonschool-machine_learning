443a81268b00f9c2fb510e2bb9d0ddcc1de7a47c
IMickisz <ivan.mickisz3@gmail.com>
2023-02-09 15:57:16 +0100

    added tasks 3, 4 and 5

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Module that contains the Exponential class """
+e = 2.7182818285
+
+
+class Exponential:
+    """ Class  that represents an exponential distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Class constructor
+           - data is a list of the data to be used to estimate the distribution
+           - lambtha is the expected number of occurences in a given time frame
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                mean = sum(data) / len(data)
+                self.lambtha = float(1 / mean)
+
+    def pdf(self, x):
+        """
+        Instance method that calculates the value of the PDF for a given time
+        period
+           - x: time period
+        Return: the PDF value for x or 0 if x is out of range
+        """
+        if x < 0:
+            return 0
+        pdf = self.lambtha * (e ** (- self.lambtha * x))
+        return pdf
+
+    def cdf(self, x):
+        """
+        Instance method that calculates the value of the CDF for a given
+        period
+           - x: time period
+        Return: the CDF value for x or 0 if x is out of range
+        """
+        if x < 0:
+            return 0
+        cdf = 1 - (e ** (- self.lambtha * x))
+        return cdf
