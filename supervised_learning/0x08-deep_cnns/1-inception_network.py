dd02a37ca1976950544d58a8d878de76b0be64af
IMickisz <ivan.mickisz3@gmail.com>
2023-02-10 21:46:02 +0100

    removed problematic layer

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -17,8 +17,6 @@ def inception_network():
     output = K.layers.MaxPooling2D(
         pool_size=(3, 3), strides=(2, 2), padding='same')(output)
     output = K.layers.Conv2D(
-        64, 1, strides=(1, 1), padding='same', activation='relu')(output)
-    output = K.layers.Conv2D(
         192, 3, strides=(1, 1), padding='same', activation='relu')(output)
     output = K.layers.MaxPooling2D(
         pool_size=(3, 3), strides=(2, 2), padding='same')(output)
@@ -41,7 +39,8 @@ def inception_network():
     output = inception_block(output, [256, 160, 320, 32, 128, 128])
     output = inception_block(output, [384, 192, 384, 48, 128, 128])
 
-    output = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1))(output)
+    output = K.layers.AveragePooling2D(
+        pool_size=(7, 7), strides=(1, 1))(output)
     output = K.layers.Dropout(rate=0.4)(output)
     output = K.layers.Dense(units=1000, activation='softmax')(output)
 
