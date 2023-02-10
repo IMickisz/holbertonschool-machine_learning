0fee67bae674ba870a898c42b3f3dfd2d51b5bcc
IMickisz <ivan.mickisz3@gmail.com>
2023-02-10 21:41:44 +0100

    added tasks 2 to 7

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Module that contains identity_block function """
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """
+    Function that builds the inception network
+        A_prev: output from the previous layer
+        filters: tuple or list containing F11, F3, F12, respectively:
+            F11: number of filters in the first 1x1 convolution
+            F3: number of filters in the 3x3 convolution
+            F12: number of filters in the second 1x1 convolution
+    Return: the activated output of the identity block
+    """
+    F11, F3, F12 = filters
+
+    initializer_kernel_weights = K.initializers.HeNormal(seed=None)
+
+    output_layer = K.layers.Conv2D(
+        filters=F11, kernel_size=(1, 1), strides=(1, 1), padding='same',
+        kernel_initializer=initializer_kernel_weights)(A_prev)
+    batch_normalization = K.layers.BatchNormalization(axis=3)(output_layer)
+    activated_output = K.layers.Activation('relu')(batch_normalization)
+
+    output_layer = K.layers.Conv2D(
+        filters=F3, kernel_size=(3, 3), padding='same',
+        kernel_initializer=initializer_kernel_weights)(activated_output)
+    batch_normalization = K.layers.BatchNormalization(axis=3)(output_layer)
+    activated_output = K.layers.Activation('relu')(batch_normalization)
+
+    output_layer = K.layers.Conv2D(
+        filters=F12, kernel_size=(1, 1), padding='same',
+        kernel_initializer=initializer_kernel_weights)(activated_output)
+    batch_normalization = K.layers.BatchNormalization(axis=3)(output_layer)
+    add = K.layers.Add()([batch_normalization, A_prev])
+    activated_output = K.layers.Activation('relu')(add)
+
+    return activated_output
