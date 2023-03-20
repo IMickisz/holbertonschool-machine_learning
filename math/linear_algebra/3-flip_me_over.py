cabdc46e660c69dd0a46bca47b4e61d35e8fa603
IMickisz <ivan.mickisz3@gmail.com>
2023-03-20 11:18:36 +0100

    added task 3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module that contains matrix_transpose """
+
+
+def matrix_transpose(matrix):
+    """ Function that the transpose of a 2D matrix """
+    matrix_t = []
+    for i in range(len(matrix[0])):
+        row = []
+        for item in matrix:
+            row.append(item[i])
+        matrix_t.append(row)
+    return matrix_t
