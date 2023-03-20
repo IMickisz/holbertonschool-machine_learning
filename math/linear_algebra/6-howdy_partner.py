893140ec190bc6a08f448893a68d66c963dde4c4
IMickisz <ivan.mickisz3@gmail.com>
2023-03-20 11:43:42 +0100

    added task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that contains cat_arrays function """
+
+
+def cat_arrays(arr1, arr2):
+    """ Function that concatenates two arrays """
+    array = arr1 + arr2
+    return array
