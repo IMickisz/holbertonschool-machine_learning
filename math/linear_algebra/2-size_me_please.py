7f3396bd5dfafead77837c586e7212ce931eab05
IMickisz <ivan.mickisz3@gmail.com>
2023-03-20 11:10:43 +0100

    added task 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module that contains matrix_shape """
+
+
+def matrix_shape(matrix):
+    """ Function that calculates the shape of a matrix """
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
